f7e53899994d4d56c5c3781d6477ee3f7d629a8d
FacuM <facumo.fm@gmail.com>
2019-01-13 04:36:00 -0300

    Support for ebooks (epub), block devices, sockets, unkown special files and more. Fix newlines.

--- aliases.sh
+++ aliases.sh
@@ -11,11 +11,35 @@
 #
 ########################################
 
+# Get the path to the script.
+ME=$(dirname $(realpath $0))
+#   /\        /\
+#   ||        ||
+#
+# According to https://bit.ly/2OjAUd9
+
 # Arrays
 EMOJIS=(
 '\xF0\x9F\x93\x84' # (page facing up)
 '\xF0\x9F\x93\x81' # (file folder)
 '\xF0\x9F\x93\x9F' # (pager)
+'\xF0\x9F\x92\xBD' # (minidisc)
+'\xF0\x9F\x93\x9C' # (scroll)
+'\xF0\x9F\x92\xBB' # (personal computer)
+'\xE2\x99\xA8'     # (hot springs)
+'\xE2\x9D\x93'     # (black question mark ornament)
+)
+
+EARTHS=(
+'\xF0\x9F\x8C\x8D' # (earth globe europe-africa)
+'\xF0\x9F\x8C\x8E' # (earth globe america)
+)
+
+BOOKS=(
+'\xF0\x9F\x93\x95' # (closed book)
+'\xF0\x9F\x93\x97' # (green book)
+'\xF0\x9F\x93\x98' # (blue book)
+'\xF0\x9F\x93\x99' # (orange book)
 )
 
 CLOCKS=(
@@ -34,6 +58,16 @@ CLOCKS=(
 )
 
 # Functions
+function getEarthEmoji
+{
+ printf "${EARTHS[ $(( $RANDOM % 2 + 1 )) ]}"
+}
+
+function getBookEmoji
+{
+ printf "${BOOKS[ $(( $RANDOM % 2 + 1 )) ]}"
+}
+
 function clear_line
 {
   printf '\r'
@@ -49,28 +83,63 @@ function clear_line
 function alias_ls
 {
    OUT=''; OUT_LS=$(find $1 -maxdepth 1)
+   # IFS setup, end of field ONLY if newline found.
+   IFS=$'\n'
    for content in $OUT_LS
    do
-    . includes/do_anim_cycle.sh
+    . "$ME"/includes/do_anim_cycle.sh
     SEEK=$(echo "$content" | tr -cd '/' | wc -c)
     CURRENT=$(echo "$content" | cut -d '/' -f $(( $SEEK + 1 )) )
     # Regular file
     if [ -f "$content" ]
     then
-     OUT="$OUT""${EMOJIS[0]}"' '"$CURRENT\n"
+     if [ -s "$content" ]
+     then
+      SECONDSEEK=$(echo "$content" | tr -cd '.' | wc -c)
+      SECONDCURRENT=$(echo "$content" | cut -d '.' -f $(( $SECONDSEEK + 1 )) | cut -d '/' -f $(( $SECONDSEEK + 1 )))
+      case "$SECONDCURRENT" in
+        'LICENSE')
+          OUT="$OUT""${EMOJIS[4]}"' '"$SECONDCURRENT"
+          ;;
+        'sh')
+          OUT="$OUT""${EMOJIS[5]}"' '"$SECONDCURRENT"
+          ;;
+        'epub')
+          OUT="$OUT""$(getBookEmoji)"' '"$SECONDCURRENT"
+          ;;
+        *)
+          OUT="$OUT""${EMOJIS[0]}"' '"$SECONDCURRENT"
+          ;;
+      esac
+     else
+      OUT="$OUT""${EMOJIS[0]}"' '"$CURRENT"
+     fi
     else
      # Directory
      if [ -d "$content" ]
      then
-      OUT="$OUT""${EMOJIS[1]}"' '"$CURRENT\n"
+      OUT="$OUT""${EMOJIS[1]}"' '"$CURRENT"
      else
       # Special file
       if [ -c "$content" ]
       then
-       OUT="$OUT""${EMOJIS[2]}"' '"$CURRENT\n"
+       OUT="$OUT""${EMOJIS[2]}"' '"$CURRENT"
+      else
+       if [ -b "$content" ]
+       then
+        OUT="$OUT""${EMOJIS[3]}"' '"$CURRENT"
+       else
+        if [ -S "$content" ]
+        then
+         OUT="$OUT""$(getEarthEmoji)"' '"$CURRENT"
+        else
+         OUT="$OUT""${EMOJIS[7]}"' '"$CURRENT"
+	fi
+       fi
       fi
      fi
     fi
+    OUT="$OUT"'\n'
   done
   clear_line
   printf "$OUT"
@@ -78,4 +147,9 @@ function alias_ls
 
 # Main
 printf '\n'
-alias_ls $1
+if [ -t 1 ]
+then
+ alias_ls $1
+else
+ ls $1
+fi
